6719ba8b0d70ba27a583e24e82e6a5a3fe53c979
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-03-07 12:23:34 -0500

    copied timer functions from timer activity to set up rudimentary clock

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,23 +9,80 @@
 // 9) On the end screen our timer is stopped, the screen wont change unless the player clicks the "play again" button.
 // 10) If the player clicks the "play again" button the game re-initializes, it DOES NOT reset.
 
+// Start Screen Variables
 $("#question-container").hide();
+$("#timer-container").hide();
 var gameisStarted = false;
 
+// Timer Variables
+var clockRunning = false;
+var time = 0;
+
+// Timer Functions
+function start() {
+
+    // DONE: Use setInterval to start the count here and set the clock to running.
+    if (!clockRunning) {
+        intervalId = setInterval(count, 1000);
+        clockRunning = true;
+    }
+}
+function stop() {
+
+    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+    clearInterval(intervalId);
+    clockRunning = false;
+}
+function count() {
+
+    // DONE: increment time by 1, remember we cant use "this" here.
+    time++;
+
+    // DONE: Get the current time, pass that into the timeConverter function,
+    //       and save the result in a variable.
+    var converted = timeConverter(time);
+    console.log(converted);
+
+    // DONE: Use the variable we just created to show the converted time in the "display" div.
+    $("#timer-text").text(converted);
+}
+function timeConverter(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+        minutes = "00";
+    }
+    else if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+}
+
 // When player clicks start
-$("#start-button").on("click", function() {
+$("#start-button").on("click", function () {
     //Flip game started flag
     gameisStarted = true;
     // Hide instructions
     $("#instructions-container").hide();
     // Display Questions
     $("#question-container").show();
+    // Display Timer
+    $("#timer-container").show();
+    // Start timer
+    start();
 })
 
 // During gameplay make the list items clickable
 if (gameisStarted = true) {
     // Make list items clickable
-    $("li").on("click", function() {
+    $("li").on("click", function () {
         alert("clicked!")
     })
 }

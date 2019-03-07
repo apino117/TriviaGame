145bb2b0193d065ca1a9dd3521b8bd444a73851b
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-03-07 11:14:34 -0500

    set up files, psuedocoded javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// 1) Start screen, instructions and start button 
+// 2) Player hits start, game begins 
+// 3) When game begins, 30 second timer starts to count down, question is displayed
+// 4) While the timer is running the player may make a selection by clicking on one of the answers. If the timer runs out the question is marked as unanswered. 
+// 5) When the player makes a selection the screen changes and displays accordingly "right" or "wrong." With an image that matches the correct answer. 
+// 6) After five seconds of that screen we load another question and repeat the process. 
+// 7) After the requisite amount of questions are answered we go to the end screen. 
+// 8) The end screen displays the results: correct answers, incorrect and the unanswered. There's also a button to play again.
+// 9) On the end screen our timer is stopped, the screen wont change unless the player clicks the "play again" button.
+// 10) If the player clicks the "play again" button the game re-initializes, it DOES NOT reset.
